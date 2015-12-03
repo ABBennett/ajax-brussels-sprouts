49d6e1ea0a2637ae30a6ead0ce681331305df222
AlexBennett <alexanderbondbennett@gmail.com>
2015-12-03 08:56:09 -0500

    finished challenge

---
+++ public/javascripts/app.js
@@ -0,0 +1,13 @@
+$('#get-dish').on('click', function(event) {
+  event.preventDefault();
+
+  var request = $.ajax({
+    method: "GET",
+    url: "/.json"
+  });
+
+
+  request.done(function(newDish) {
+    alert(newDish["dish"]);
+  });
+});
